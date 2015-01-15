049a8d3bc65f797ba14eb2be9e96c6e580be9bfb
David Monllao <davidm@moodle.com>
2015-01-15 13:15:05 +0800

    MDL-48838 cache: Using gradecategories cache

--- lang/en/cache.php
+++ lang/en/cache.php
@@ -47,6 +47,7 @@ $string['cachedef_databasemeta'] = 'Database meta information';
 $string['cachedef_eventinvalidation'] = 'Event invalidation';
 $string['cachedef_externalbadges'] = 'External badges for particular user';
 $string['cachedef_suspended_userids'] = 'List of suspended users per course';
+$string['cachedef_gradecategories'] = 'List of grade categories';
 $string['cachedef_gradecondition'] = 'User grades cached for evaluating conditional availability';
 $string['cachedef_groupdata'] = 'Course group information';
 $string['cachedef_htmlpurifier'] = 'HTML Purifier - cleaned content';
